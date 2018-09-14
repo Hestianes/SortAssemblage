063e6b60dbe052b137664e36620c0936ae9a7428
Hestianes <40913569+Hestianes@users.noreply.github.com>
2018-09-14 14:55:32 +0800

    Update
    Update finished MergeSort  into the assemblage.

--- SortAssemblage.cpp
+++ SortAssemblage.cpp
@@ -9,16 +9,19 @@ class SortAssemblage
 public:
 	SortAssemblage();
 	~SortAssemblage();
+	void CppTest();
 
 	void ShowSequence();
 	void BuildSequence();
 	void InsertionSort();
 	void SelectionSort();
 	void QuickSort();
+	void MergeSort();
 
 private:
 	int sequence[50];
 	void atom_QuickSort(int head, int tail);
+	void atom_MergeSort(int head, int tail);
 };
 
 SortAssemblage::SortAssemblage()
@@ -37,6 +40,11 @@ SortAssemblage::SortAssemblage()
 SortAssemblage::~SortAssemblage()
 {
 	cout << endl << "destruction called." << endl;
+}
+
+void SortAssemblage::CppTest()
+{
+	cout << 7 / 2 << 7 / 3;
 }
 
 void SortAssemblage::ShowSequence()
@@ -150,18 +158,88 @@ void SortAssemblage::atom_QuickSort(int head, int tail)
 	this->atom_QuickSort(SwitchPoint + 1, tail);
 }
 
+void SortAssemblage::MergeSort()
+{
+	this->atom_MergeSort(0, 49);
+}
+void SortAssemblage::atom_MergeSort(int head, int tail)
+{
+	int mid = (head + tail) / 2;
+	if ( (tail - head) > 1 )
+	{
+		this->atom_MergeSort(head, mid);
+		this->atom_MergeSort(mid + 1, tail);
+	}
+	int testA = 0, testB = 0;
+	int length = tail - head + 1;
+	int InsertPointA = head, InsertPointB = mid + 1;
+	int* tempBuff = new int[length];
+
+	if (head == tail)
+	{
+		InsertPointB = head;
+	}
+
+	for (size_t k = 0; k < length; k++)
+	{
+		if (this->sequence[InsertPointA] <= this->sequence[InsertPointB])
+		{
+			tempBuff[k] = this->sequence[InsertPointA];
+			testA = tempBuff[k];
+			InsertPointA++;
+			if (InsertPointA > mid)
+			{
+				while (k + 1 < length)
+				{
+					tempBuff[k + 1] = this->sequence[InsertPointB++];
+					testB = tempBuff[k + 1];
+					k++;
+				}
+				break;
+			}
+		}
+		else if (this->sequence[InsertPointA] > this->sequence[InsertPointB])
+		{
+			tempBuff[k] = this->sequence[InsertPointB];
+			testB = tempBuff[k];
+			InsertPointB++;
+			if (InsertPointB > tail)
+			{
+				while (k + 1 < length)
+				{
+					tempBuff[k + 1] = this->sequence[InsertPointA++];
+					testA = tempBuff[k + 1];
+					k++;
+				}
+
+				break;
+			}
+		}
+
+	}
+	//cout << endl << length << endl;
+	for (size_t j = 0; j < length; j++)
+	{
+		//cout << tempBuff[j] << '\t';
+		this->sequence[j + head] = tempBuff[j];
+
+	}
+
+
+	delete tempBuff;
+}
+
 int main()
 {
 	SortAssemblage* sortAss1 = new SortAssemblage();
 
-	sortAss1->QuickSort();
+	sortAss1->MergeSort();
 	sortAss1->ShowSequence();
 
 	sortAss1->BuildSequence();
 	sortAss1->ShowSequence();
-	sortAss1->QuickSort();
+	sortAss1->MergeSort();
 	sortAss1->ShowSequence();
-
 	delete sortAss1;
 	system("PAUSE");
 	return 0;
